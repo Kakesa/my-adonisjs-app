2fda8e560930fc1f72d7dfd16016eb05cb12b106
Espoir Kakesa <120624185+Kakesa@users.noreply.github.com>
2025-09-15 17:03:33 +0100

    fix : ajout users_controller

--- start/routes.ts
+++ start/routes.ts
@@ -8,5 +8,7 @@
 */
 
 import router from '@adonisjs/core/services/router'
+const UsersController = () => import('#controllers/users_controller')
 
 router.on('/').render('pages/home')
+router.get('/users', [UsersController, 'index'])
